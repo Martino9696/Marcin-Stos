df3831a5df746392ebe65a11705e14454ff0006c
Marcino9696 <252884@edu.p.lodz.pl>
2024-06-09 20:47:07 +0200

    initial commit

---
+++ stack.h
@@ -0,0 +1,15 @@
+//
+// Created by kopyt on 09.06.2024.
+//
+
+#ifndef UNTITLED76_STACK_H
+#define UNTITLED76_STACK_H
+
+int isStackEmpty(void);
+int isStackFull(void);
+int Pop(void);
+int Top(void);
+void Push(int val);
+
+
+#endif //UNTITLED76_STACK_H
